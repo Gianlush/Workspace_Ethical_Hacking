69e8ff7d197186b79407a312a33ceb80df7ba9d2
Gianluca Massara <62150736+Gianlush@users.noreply.github.com>
2023-09-01 16:36:08 +0200

    Create README.md

---
+++ README.d
@@ -0,0 +1,3 @@
+Cartella di lavoro per test, esempi e esercitazione del corso universitario Ethical Hacking.
+
+  Contenente varie challenge web risolte e esercitazioni di esami per sfide nello stile di Attaco e Difesa o anche boot-to-root.
